25e98d37206f15f870ad0a365ff9b30c9dfee732
Arnav Gaur <60852260+arnav8@users.noreply.github.com>
2020-09-05 12:38:30 +0530

    Update NonLinearRegression.py

--- NonLinearRegression.py
+++ NonLinearRegression.py
@@ -1,20 +1,20 @@
 #coding=utf-8
 #Date 2017.03.12
-#毕业设计程序实现
+#Graduation project realization
 
 from numpy import *
 import matplotlib.pyplot as plt
 
-'''非线性回归函数
-参数：
-x：训练集样本集合，numpy数组
-y：训练集标签集合，numpy数组
-newx：测试集样本集合，numpy数组
-newy：测试集标签集合，numpy数组
-返回值：
-train_error：训练集误差，浮点型
-error：测试集误差，浮点型
-rr：回归训练集r方
+'''Nonlinear regression function
+parameter:
+x: training set sample collection, numpy array
+y: training set label collection, numpy array
+newx: test set sample collection, numpy array
+newy: test set label collection, numpy array
+return value:
+train_error: training set error, floating point
+error: test set error, floating point
+rr: regression training set r square
 '''
 def NonLinearRegression(x, y, newx, newy):
 	xMat = x
@@ -26,7 +26,7 @@ def NonLinearRegression(x, y, newx, newy):
 	yHat = xMat * ws
 	yHat = exp(yHat)
 
-	#计算训练集r方
+	#Calculate the training set r square
 	SSE = (y - yHat).T * (y - yHat)
 	error_train = SSE / 2
 	yAvg = mean(y)
@@ -34,18 +34,18 @@ def NonLinearRegression(x, y, newx, newy):
 	SST = SSR + SSE
 	rr = SSR / SST
 
-	#计算测试集上的均方误差
+	#Calculate the mean square error on the test set
 	predict_y = newx * ws
 	predict_y = exp(predict_y)
 	error = (predict_y - newy).T * (predict_y - newy) / 2
 
-	#画图
+	#plot
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
-	#画点
+	#plot a point
 	ax.scatter(predict_y.flatten().A[0], newy.flatten().A[0], color = 'black')
 	ax.scatter(yHat.flatten().A[0], y.flatten().A[0], color = 'blue')
-	#画线
+	#plot a line
 	x = [0, 600]
 	y = [0, 600]
 	ax.plot(x, y, 'r')
