8abb8595ace02561a741099dc70311b976cd5124
Arnav Gaur <60852260+arnav8@users.noreply.github.com>
2020-09-05 12:11:52 +0530

    Update BPnn.py

--- BPnn.py
+++ BPnn.py
@@ -5,19 +5,19 @@ from Bp_train import *
 from numpy import *
 import matplotlib.pyplot as plt
 
-'''神经网络调用函数
-参数：
-x：训练集样本集合，numpy数组
-y：训练集标签集合，numpy数组
-newx：测试集样本集合，numpy数组
-newy：测试集标签集合，numpy数组
-d：布尔型变量，是否使用fw算法进行优化
-返回值：
-train_error：训练集误差，浮点型
-error：测试集误差，浮点型
+'''Neural network call function
+parameter:
+x: training set sample collection, numpy array
+y: training set label collection, numpy array
+newx: test set sample collection, numpy array
+newy: test set label collection, numpy array
+d: Boolean variable, whether to use the fireworks algorithm for optimization
+return value:
+train_error: training set error, floating point
+error: test set error, floating point
 '''
 def BPneuralnetwork(x, y, newx, newy, d):
-	#训练集归一化
+	#Normalization of training set
 	xx = zeros([shape(x)[0], shape(x)[1]])
 	yy = zeros([shape(y)[0], shape(y)[1]])
 
@@ -29,8 +29,9 @@ def BPneuralnetwork(x, y, newx, newy, d):
 		for j in range(shape(y)[1]):
 			yy[i, j] = (y[i, j] - min(y[:, j])) / (max(y[:, j] - min(y[:, j])))
 
-	#求训练集均方误差
-	#a为神经网络的迭代误差、EE为烟花算法的迭代误差
+	#Seeking mean square error of training set
+        #a is the iteration error of the neural network,
+        #EE is the iteration error of the firework algorithm
 	whj, rh, vih, thetaj, a, EE = MyBP(xx, yy, 6, d)
 	alphah = dot(xx, whj)
 	bh = sigmoid(alphah - rh)
@@ -44,33 +45,33 @@ def BPneuralnetwork(x, y, newx, newy, d):
 
 	train_error = sum((NewYY - y) * (NewYY - y)) / 2
 
-	#测试集
+	#Test set
 	newxx = zeros([shape(newx)[0], shape(newx)[1]])
-	#测试集归一化
+	#Test set normalization
 	for i in range(shape(newx)[0]):
 		for j in range(shape(newx)[1]):
 			newxx[i, j] = (newx[i, j] - min(x[:, j])) / (max(x[:, j] - min(x[:, j])))
 
-	#神经网络预测
+	#Neural network prediction
 	predict_y = predictY(newxx, whj, rh, vih, thetaj)
-	#预测值反归一化
+	#Denormalization of predicted value
 	newpredict_y = zeros([shape(predict_y)[0], shape(predict_y)[1]])
 	for i in range(shape(predict_y)[0]):
 		for j in range(shape(predict_y)[1]):
 			newpredict_y[i, j] = predict_y[i, j] * (max(y[:, j] - min(y[:, j]))) + \
 			                  min(y[:, j])
 
-	#求测试集均方误差
+	#Find the mean square error of the test set
 	error = sum((newpredict_y - newy) * (newpredict_y - newy)) / 2
 
-	#画图
-	#真实值与预测值图像
+	#plotting
+        #Real value and predicted value image
 	fig1 = plt.figure()
 	ax = fig1.add_subplot(111)
-	#画点
+																								#画点
 	ax.scatter(NewYY.flatten(), y.flatten(), color = 'blue')
 	ax.scatter(newpredict_y.flatten(), newy.flatten(), color = 'black')
-	#画线
+	#Draw a point
 	x = [0, 600]
 	y = [0, 600]
 	ax.plot(x, y, 'r')
@@ -79,7 +80,7 @@ def BPneuralnetwork(x, y, newx, newy, d):
 	plt.ylabel('realvalue', fontname='times new Roman', fontsize='10.5')
 	plt.show()
 
-	#神经网络误差函数图像
+	#Neural network error function image
 	fig2 = plt.figure()
 	bx = fig2.add_subplot(111)
 	x = range(200)
