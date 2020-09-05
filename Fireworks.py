25f2af9bd025dc72f6e4e5f962ffd9f744bb6757
Arnav Gaur <60852260+arnav8@users.noreply.github.com>
2020-09-05 12:30:26 +0530

    Update Fireworks.py

--- Fireworks.py
+++ Fireworks.py
@@ -4,71 +4,71 @@
 
 from Utils import *
 
-'''烟花算法
-目的是通过烟花算法得到较好的神经网络参数的初始值，再使用梯度下降进行迭代
-参数：
-X：训练集样本集合，numpy数组
-Y：训练节标签集合，numpy数组
-h：隐层神经元数量
-返回值：
-优化后的输入层-隐层的权重，隐层阈值，隐层-输出层权重，输出层阈值初始值，均为numpy数组
+'''Firework algorithm
+The purpose is to get better initial values of neural network parameters through the firework algorithm, and then use gradient descent for iteration
+parameter:
+X: training set sample collection, numpy array
+Y: training section label collection, numpy array
+h: the number of hidden neurons
+return value:
+The optimized input layer-hidden layer weight, hidden layer threshold, hidden layer-output layer weight, and initial value of the output layer threshold are all numpy arrays
 '''
 def FWA(X, Y, h):
 	if shape(X)[0] != shape(Y)[0]:
 		print("The line of X and Y must be same!")
 	m = shape(X)[0]; n = shape(X)[1]; l = shape(Y)[1]
 
-	#初始化算法参数
-		#火花总数
+	#Initialization algorithm parameters
+        #Sparks total
 	m = 50
-		#上限和下限
+		#Upper and lower limit
 	a = 0.8; b = 0.04
-		#爆炸幅度
+		#Explosion amplitude
 	A = 40
-		#烟花高斯爆炸个数
+		#Number of fireworks Gaussian explosions
 	mm = 5
-		#烟花维度
+		#Fireworks dimension
 	dimension = n*h + h*l + h + l
-		#烟花个数
+		#Number of fireworks
 	nn = 5
-		#最大、最小边界
+		#Maximum and minimum boundaries
 	xmin = -5; xmax = 5
 
-	#初始化烟花
+	#Initialize fireworks
 	fireworks = zeros([nn, dimension])
 	for i in range(nn):
 		for j in range(dimension):
 			fireworks[i][j] = random.uniform(-5, 5)
 
-	#初始化新烟花
+	#Initialize a new firework
 	fireworks_new = zeros([nn, 100, dimension])
 
-	#初始化高斯火花
+	#Initialize Gaussian spark
 	fireworks_rbf = zeros([nn, dimension])
 
-	#产生火花
-		#每个烟花产生火花的数量
+	#Sparks
+        #The number of sparks produced by each firework
 	Si = zeros([nn, 1])
-		#每个烟花的爆炸半径
+		#Explosion radius of each firework
 	Ai = zeros([nn, 1])
-		#火花限制
+		#Spark limit
 	si = zeros([nn, 1])
-		#计算每个烟花的适应度函数值
+		#Calculate the fitness function value of each firework
 	f = zeros([nn, 1])
-		#最大最小适应度
+		#Maximum and minimum fitness
 	fmax = f[0]; fmin = f[nn-1]
-		#误差函数初始化
+		#Error function initialization
 	E = zeros([5000, 1])
 
 
-	#烟花算法迭代过程
+	#Firework algorithm iteration process
 	for delta_num in range(5000):
 
-		# 普通爆炸产生的火花总数
+		# Total number of sparks produced by ordinary explosions
 		sum_new_fireworks = 0
-		# 总适应度
+		# Total fitness
 		sum = 0
-		#计算适应度,并求出最大值和最小值
+		#Calculate fitness and find the maximum and minimum
 		for i in range(nn):
 			f[i] = calculatef(X, Y, fireworks[i], n, h, l)
 			if f[i] > fmax:
@@ -77,9 +77,9 @@ def FWA(X, Y, h):
 				fmin = f[i]
 			sum += f[i]
 
-			#求每个烟花的爆炸半径和火花数
+			#Find the explosion radius and number of sparks for each firework
 		for i in range(nn):
-				#计算火花数
+				#Calculate the number of sparks
 			Si[i] = m * (fmax - f[i] + 0.0001) / (nn * fmax - sum + 0.0001)
 			Si[i] = round(Si[i])
 			if Si[i] < a * m:
@@ -88,45 +88,45 @@ def FWA(X, Y, h):
 				si[i] = round(b * m)
 			else:
 				si[i] = round(Si[i])
-				#不能超过火花数限制
+				#Can not exceed the number of sparks limit
 			if Si[i] > si[i]:
 				Si[i] = si[i]
 
-				#计算普通爆炸产生的火花总数
+				#Calculate the total number of sparks produced by ordinary explosions
 			sum_new_fireworks += int(Si[i])
 
-			#计算爆炸半径
+			#Calculate the explosion radius
 			Ai[i] = A * (f[i] - fmin + 0.0001) / (sum - nn * fmin + 0.0001)
 
-				#产生新火花
+				#Create a new spark
 			for j in range(Si[i]):
-					#初始化新火花
+					#Initialize a new spark
 				fireworks_new[i][j] = fireworks[i]
-					#随机选择z个维度
+					#Randomly select z dimensions
 				z = random.randint(1, dimension)
-					#打乱随机选择前z个
+					#Randomly select the first z
 				zz = range(dimension)
 				random.shuffle(zz)
 
-					# 产生新火花
+					# Create a new spark
 				for k in range(z):
 					fireworks_new[i][j][zz[k]] += random.uniform(0, Ai[i])
 
 
-		#产生高斯火花（每个烟花产生一个高斯火花）
-			# 随机选择z个维度
+		#Generate Gaussian sparks (each firework generates a Gaussian spark)
+			# Randomly select z dimensions
 		z = random.randint(1, dimension)
 		zz = range(dimension)
 		random.shuffle(zz)
-			#高斯随机数
+			#Gaussian random number
 		g = random.uniform(-1, 1)
-			#高斯爆炸算子
+			#Gaussian explosion operator
 		for i in range(mm):
 			for j in range(z):
 				fireworks_rbf[i][zz[j]] = g * fireworks[i][zz[j]]
 
 
-			#构造总烟花
+			#Construct total fireworks
 		sum_fireworks = nn + sum_new_fireworks + mm
 		fireworks_final = zeros([sum_fireworks, dimension])
 		for i in range(nn):
@@ -144,22 +144,22 @@ def FWA(X, Y, h):
 			fireworks_final[int(nn+sum_new_fireworks+i)] = fireworks_rbf[i]
 
 
-		#映射规则
+		#Mapping rule
 		for i in range(sum_fireworks):
 			for j in range(dimension):
 				if fireworks_final[i][j] > xmax or fireworks_final[i][j] < xmin:
 					fireworks_final[i][j] = xmin + mod(abs(fireworks_final[i][j]), \
 				                           (xmax - xmin))
 
-		# 选择策略
-			#爆炸后新种群适应度
+		#Choose a strategy
+                #New population fitness after explosion
 		f_new = zeros([sum_fireworks, 1])
 		f_new_min = f_new[0]
 		#print f_new_min
-			#初始化最优适应度下标
+			#Initialize the optimal fitness index
 		min_i = 0
-			#选出下一代nn个个体，由最大适应度个体与距离更远的nn-1个个体组成
-			#求最优适应度
+			#Select the next generation of n individuals, consisting of the maximum fitness individual and nn-1 individuals farther away
+			 #Find the optimal fitness
 		for i in range(sum_fireworks):
 			#print fireworks_final[i]
 			f_new[i] = calculatef(X, Y, fireworks_final[i], n, h, l)
@@ -168,26 +168,26 @@ def FWA(X, Y, h):
 				min_i = i
 
 
-			#求出每个个体被选择的概率
-			#初始化两两个体之间的距离
+			#Find the probability of each individual being selected
+                         #Initialize the distance between two bodies
 		D = zeros([sum_fireworks, sum_fireworks])
-			#计算两两个体之间的距离
+			#Calculate the distance between two bodies
 		for i in range(sum_fireworks):
 			for j in range(sum_fireworks):
 				D[i][j] = dot((fireworks_final[i] - fireworks_final[j]), \
 				              (fireworks_final[i] - fireworks_final[j])) / 2
 
-			#初始化每个个体与其他个体之间的距离之和
+			#Initialize the sum of the distance between each individual and other individuals
 		Ri = zeros([sum_fireworks, 1])
-			#初始化距离矩阵的副本
+			#Initialize a copy of the distance matrix
 		RRi = zeros([sum_fireworks, 1])
-			#计算每个个体与其他个体的距离之和
+			#Calculate the sum of the distance between each individual and other individuals
 		for i in range(sum_fireworks):
 			for j in range(sum_fireworks):
 				Ri[i] += D[i][j]
 		RRi = Ri
 
-			#选出距离最远的nn-1个个体，即对距离矩阵进行排序
+			#Select nn-1 individuals with the farthest distance, that is, sort the distance matrix
 		for i in range(sum_fireworks-1):
 			for j in range(i, sum_fireworks):
 				if Ri[i] < Ri[j]:
@@ -195,7 +195,7 @@ def FWA(X, Y, h):
 					Ri[i] = Ri[j]
 					Ri[j] = tmp
 
-			#构造新种群
+			#Construct a new population
 		fireworks[0] = fireworks_final[min_i]
 		for i in range(sum_fireworks):
 			if Ri[0] == RRi[i]:
@@ -207,8 +207,8 @@ def FWA(X, Y, h):
 			if Ri[3] == RRi[i]:
 				fireworks[4] = fireworks_final[i]
 
-			#迭代完毕，返回最优个体
-			#初始化最优适应度下标
+			#After iteration, return the best individual
+                     #Initialize the optimal fitness index
 		ii = 0
 		for i in range(nn):
 			f[i] = calculatef(X, Y, fireworks[i], n, h, l)
